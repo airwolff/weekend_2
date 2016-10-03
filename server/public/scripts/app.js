4ba8bfec54b698373d42154626041b1bda36f4fa
Andy Wolff <andy.wolff@gmail.com>
2016-10-03 14:10:42 -0500

    significant changes throughout

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,44 +1,87 @@
 $( document )
 	.ready( function () {
 
+
 		$.ajax( {
-			type: "GET",
-			url: "/data",
-			success: function ( data ) {
-				console.log( 'GET/data returns ', data );
+				type: 'GET',
+				url: '/data',
+				success: function ( data ) {
+						console.log( 'GET/data returns ', data );
 
+						var i = 0;
+						var backInt = null;
+						var aheadInt = null;
 
-				var i = 0;
-				var divs = "<div id='name'></div>" +
-					"<div id='userName'></div>" +
-					"<div id='shout'></div>";
+						$( '#prev' )
+							.on( 'click', backward );
 
-				$( "p" )
-					.append( divs );
+						$( '#next' )
+							.on( 'click', forward );
 
-				function info( data[ i ] ) {
-					$( "#name" )
-						.text( "<p>" + this.name + "</p>" );
-					$( "#userName" )
-						.text( "<p>" + this.githubUserName + "</p>" );
-					$( "#shout" )
-						.text( "<p>" + this.shoutout + "</p>" );
-					clearTimeout( timer );
-					var timer = setTimeout( forward, 3000 );
-
-				} // end function info
-				function forward() {
-					if ( data[ i ] > 17 ) {
-						data[ i ] = 0;
 						info( data[ i ] );
-						i++;
-					} else if ( data[ i ] >= 0 ) {
-						i++;
-						append( data[ i ] );
 
-					}
-				}
+						function info( data ) {
+							$( '#name' )
+								.text( data.name );
+							$( '#userName' )
+								.text( data.githubUserName );
+							$( '#shout' )
+								.text( data.shoutout );
+							// $( '.text' )
+							// 	.fadeIn( 'fast' );
+						} // end function info
 
-			} // end function success
-		} ); // end ajax
-	} ); // end ready function
+
+
+						function forward() {
+							if ( i === data.length - 1 ) {
+								i = 0;
+								info( data[ i ] );
+								i++;
+								fastFoward()
+							} else {
+								i++;
+								info( data[ i ] );
+								fastFoward();
+							}
+						} // end function forward
+
+
+						function backward() {
+							if ( i === 0 ) {
+								i = data.length - 1;
+								info( data[ i ] );
+								rewind()
+							} else {
+								i--;
+								info( data[ i ] );
+								rewind()
+							}
+						} // end function backward
+
+						function rewind() {
+							if ( backInt ) {
+								clearInterval( backInt );
+								clearInterval( aheadInt );
+								// $( '.text' )
+								// 	.fadeOut( 'slow' );
+							}
+							backInt = setInterval( backward, 9000 );
+						} // end function rewind
+
+						function fastFoward() {
+							if ( aheadInt ) {
+								clearInterval( aheadInt );
+								clearInterval( backInt );
+								// $( '.text' )
+								// 	.fadeOut( 'slow' );
+							}
+							aheadInt = setInterval( forward, 9000 );
+						}
+					} // end function fastFoward
+
+			} ) // end function success
+
+	} ); // end ajax
+
+// end ready function
